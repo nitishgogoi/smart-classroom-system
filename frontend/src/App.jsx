6bf6ecb8bc75bdc64478c872a1988dfaa89dbdb2
Nitish <vngnats@gmail.com>
2026-05-26 00:15:16 +0530

    added teacher route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,192 +1,47 @@
-import {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-BrowserRouter,
-Routes,
-Route
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-}
+import StudentDashboard from "./pages/StudentDashboard";
+import TeacherDashboard from "./pages/TeacherDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 
-from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
 
-import Login
-from "./pages/Login";
+      <Routes>
 
-import UploadNotes
-from "./pages/UploadNotes";
+        <Route
+          path="/"
+          element={<Login />}
+        />
 
-import Register
-from "./pages/Register";
+        <Route
+          path="/register"
+          element={<Register />}
+        />
 
-import TeacherDashboard
-from "./pages/TeacherDashboard";
+        <Route
+          path="/student"
+          element={<StudentDashboard />}
+        />
 
-import StudentDashboard
-from "./pages/StudentDashboard";
+        <Route
+          path="/teacher"
+          element={<TeacherDashboard />}
+        />
 
-import Attendance
-from "./pages/Attendance";
+        <Route
+          path="/admin"
+          element={<AdminDashboard />}
+        />
 
-import Assignments
-from "./pages/Assignments";
+      </Routes>
 
-import Notes
-from "./pages/Notes";
-
-import Grades
-from "./pages/Grades";
-
-import ProtectedRoute
-
-from "./components/ProtectedRoute";
-
-import Subjects
-from "./pages/Subjects";
-
-import SubmitAssignment
-from "./pages/SubmitAssignment";
-
-
-
-function App(){
-
-return(
-
-<BrowserRouter>
-
-<Routes>
-
-
-
-<Route
-
-path="/"
-
-element={<Login/>}
-
-/>
-
-<Route
-
-path="/upload-notes"
-
-element={
-<UploadNotes/>
-}
-
-/>
-
-
-
-<Route
-
-path="/register"
-
-element={<Register/>}
-
-/>
-
-
-
-<Route
-
-path="/teacher"
-
-element={
-
-<ProtectedRoute
-role="teacher"
->
-
-<TeacherDashboard/>
-
-</ProtectedRoute>
-
-}
-
-/>
-
-
-
-
-<Route
-
-path="/student"
-
-element={
-
-<ProtectedRoute
-role="student"
->
-
-<StudentDashboard/>
-
-</ProtectedRoute>
-
-}
-
-/>
-
-
-
-<Route
-
-path="/attendance"
-
-element={<Attendance/>}
-
-/>
-
-
-
-<Route
-
-path="/assignments"
-
-element={<Assignments/>}
-
-/>
-
-<Route
-path="/submit-assignment"
-element={<SubmitAssignment/>}
-/>
-
-
-
-<Route
-
-path="/notes"
-
-element={<Notes/>}
-
-/>
-
-
-
-<Route
-
-path="/grades"
-
-element={<Grades/>}
-
-/>
-
-<Route
-
-path="/subjects"
-
-element={<Subjects/>}
-
-/>
-
-
-
-</Routes>
-
-</BrowserRouter>
-
-)
-
+    </BrowserRouter>
+  );
 }
 
 export default App;
