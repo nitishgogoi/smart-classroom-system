bb38b979938999efc41d6744207aa9b075a80038
Nitish <vngnats@gmail.com>
2026-05-26 02:55:53 +0530

    Fixed blank screen: wrapped App in BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- 1. Add this import
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* <-- 2. Wrap your App component */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
